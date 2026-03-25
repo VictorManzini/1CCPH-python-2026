1c062ad32454e2af61759f9350c07468337a33ee
Victor.Manzini <victor@robertomanzini.com.br>
2026-03-25 08:53:40 -0300

    Corrigido pós CP1

--- semestre1/CP1/CP1.2.py
+++ semestre1/CP1/CP1.2.py
@@ -4,9 +4,26 @@ valor_hora = float(input(f"Digite o valor da hora de trabalho do(a) colaborador(
 bonus = float(input(f"Digite o valor do bônus (em reais) do(a) colaborador(a) {colaborador} (digite apenas números): "))
 desc = float(input(f"Digite o desconto do(a) colaborador(a) {colaborador} digite apenas números: "))
 
-Salário_bruto = float(horas_trab) * float(valor_hora) + float(bonus)
-Salário_liquido = float(Salário_bruto) - float(desc)
+Salário_bruto = horas_trab * valor_hora + bonus
+Salário_liquido = Salário_bruto - desc
+
 
 print(f"""Colaborador(a): {colaborador}
 Salário bruto: {Salário_bruto:.2f} R$ 
 Salário líquido: {Salário_liquido:.2f} R$""")
+
+
+# Abaixo temos o bloco antigo, o professor descontou 0.5 pontos pelo mesmo motivo do CP1.py
+'''Salário_bruto = float(horas_trab) * float(valor_hora) + float(bonus)
+Salário_liquido = float(Salário_bruto) - float(desc)'''
+
+# Bloco abaixo é apenas uma variação do código onde eu coloco o valor sem o bônus para o usuario visualizar
+
+'''Salário_s_bonus = horas_trab * valor_hora
+
+print(f"""Colaborador(a): {colaborador}
+Salário bruto sem bônus: {Salário_s_bonus:.2f} R$
+Salário bruto: {Salário_bruto:.2f} R$ 
+Salário líquido: {Salário_liquido:.2f} R$""")
+
+'''
--- semestre1/CP1/CP1.py
+++ semestre1/CP1/CP1.py
@@ -4,13 +4,20 @@ preco_uni = float(input("Digite o valor unitário do produdo (digite apenas núm
 quant = int(input("Digite a quantidade comrprada de produtos: "))
 desconto = float(input("Digite o percentual de descsonto do produto (digite apenas números, ex: 15 para 15%): "))
 
-valor_bruto = float(preco_uni) * int(quant)
-desc = float(desconto) / 100
-desc_final = float(valor_bruto) * float(desc)
-valor_final = float(valor_bruto) - float(desc_final)
+valor_bruto = preco_uni * quant
+desc = desconto / 100
+desc_final = valor_bruto * desc
+valor_final = valor_bruto - desc_final
 print(f"""O produto é: {produto}
 O valor bruto é: {valor_bruto:.2f} R$
 O desconto é de: {desc_final:.2f} R$ 
 O valor final é: {valor_final:.2f} R$""")
 
+
+# Abaixo é o bloco antigo, o professor descontou 0.5 pontos devido à repetição da definição do tipo de número,
+# float ou int
+'''valor_bruto = float(preco_uni) * int(quant)
+desc = float(desconto) / 100
+desc_final = float(valor_bruto) * float(desc)
+valor_final = float(valor_bruto) - float(desc_final)'''
 
